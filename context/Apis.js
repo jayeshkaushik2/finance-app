287763477741c551c992bbe5bb22a754fe1a6f30
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-12-24 22:13:08 +0530

    fixes

--- context/Apis.js
+++ context/Apis.js
@@ -22,6 +22,7 @@ export async function registerUser(info) {
   return resp_json;
 }
 
+// added Create API context functionality
 function createRequest(request_method, request_data, token, is_default = true) {
   // creates the request info for the fetch request.
   let req = {};
@@ -43,24 +44,32 @@ function createRequest(request_method, request_data, token, is_default = true) {
     request_data !== undefined &&
     (request_method === "POST" || request_method === "PATCH")
   ) {
-    req["body"] = request_data;
+    req["body"] = JSON.stringify(request_data);
   }
   req["headers"] = headers;
+  return req;
 }
 
 function updateUrlFilter(url, filters) {
   // update the filters to url
   let new_url = url;
   let is_first = true;
-  for (var key in filters) {
-    if (is_first === true) {
-      new_url += `?${key}=${filters[key]}`;
-      is_first = false;
-    } else {
-      new_url += `&${key}=${filters[key]}`;
+  if (filters !== null) {
+    let is_changed = false;
+    for (var key in filters) {
+      if (is_first === true) {
+        new_url += `?${key}=${filters[key]}`;
+        is_first = false;
+      } else {
+        new_url += `&${key}=${filters[key]}`;
+      }
+      is_changed = true;
+    }
+    if (is_changed == true) {
+      new_url += "/";
     }
   }
-  new_url += "/";
+  console.log("url", new_url, url);
   return new_url;
 }
 
@@ -72,8 +81,10 @@ export async function CreateApiContext(
   token = null
 ) {
   // create the API context, sends the request to API.
-  let URL = updateUrlFilter(`${API_URL}${api_point}`, filters);
+  let URL = `${API_URL}${api_point}`;
+  URL = updateUrlFilter(URL, filters);
   let req = createRequest(request_method, request_data, token);
+  console.log(req);
   let resp = await fetch(URL, req);
   let resp_json = await resp.json();
   return resp_json;
