14184c51c3339d4a171daa5cb68fa74e1d975008
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2023-02-12 13:51:42 +0530

    added profile image in header

--- components/AppBar.js
+++ components/AppBar.js
@@ -1,12 +1,40 @@
 import { Appbar, Avatar, Text, TouchableRipple } from "react-native-paper";
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar, View } from "react-native";
+import AuthContext from "../context/AuthContext";
+import { CreateApiContext } from "../context/Apis";
 
 function AppBar(props) {
   const handleProfileClick = () => {
     console.log("pressed", props);
     props?.navigation?.navigate("profile");
   };
+  const { user, AuthTokens } = React.useContext(AuthContext);
+  let token = AuthTokens?.access;
+
+  const [profileData, setProfileData] = React.useState(null);
+
+  const getProfileData = async () => {
+    console.log("running api for profile data");
+    if (user === null || token === null) {
+      return;
+    }
+    const response = await CreateApiContext(
+      "/user-details/",
+      "get",
+      null,
+      null,
+      token
+    );
+    let data = await response.json();
+    if (response.ok) {
+      console.log("profile dasta", data);
+      setProfileData(data);
+    }
+  };
+  useEffect(() => {
+    getProfileData();
+  }, []);
 
   return (
     <View
@@ -35,14 +63,27 @@ function AppBar(props) {
           }}
         />
         <TouchableRipple onPress={() => handleProfileClick()}>
-          <Avatar.Image
-            size={40}
-            source={require("../assets/profile.png")}
-            style={{
-              marginTop: -20,
-              backgroundColor: "white",
-            }}
-          />
+          {profileData?.profile_image !== null ? (
+            <Avatar.Image
+              size={40}
+              source={{ uri: profileData?.profile_image }}
+              style={{
+                marginTop: -20,
+                backgroundColor: "white",
+                display: `${user === null ? "none" : "flex"}`,
+              }}
+            />
+          ) : (
+            <Avatar.Image
+              size={40}
+              source={require("../assets/profile.png")}
+              style={{
+                marginTop: -20,
+                backgroundColor: "white",
+                display: `${user === null ? "none" : "flex"}`,
+              }}
+            />
+          )}
         </TouchableRipple>
       </Appbar.Header>
     </View>
